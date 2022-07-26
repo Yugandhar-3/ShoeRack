4b44476fc52028c0e769901a969d36ab18c2a5ad
Mahipathi Yugandhar <mahipathiyugandhar@Mahipathis-MacBook-Pro.local>
2022-07-26 14:53:45 +0530

    Initial commit

---
+++ src/main/java/com/example/ShoeRack_Backend/ShoeRackBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.ShoeRack_Backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoeRackBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShoeRackBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ShoeRack_Backend/ShoeRackBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ShoeRack_Backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoeRackBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
